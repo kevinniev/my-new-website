eec1a89329b07af1dc0b9c4982d6ea5ba9797519
kevinniev <kevinniev1@gmail.com>
2026-03-02 21:31:37 -0700

    Create frontend/App.jsx with main React component

---
+++ frontend/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to My New Website!</h1>
+            <p>This is the main App component.</p>
+        </div>
+    );
+};
+
+export default App;
